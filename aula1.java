480214b404ccf1daabe5ecd0f1cf8792df4102de
bruno <heloisa.brandaodocouto@gmail.com>
2023-04-14 13:55:46 -0300

    java

---
+++ aula1.java
@@ -0,0 +1,18 @@
+// -> È um comentário de 1 linha 
+
+/*
+ Esse é o nosso 1° programa em Java
+ Ele fará a impressão de uma mensagem na tela*/
+
+public class aula1{
+
+//Método main: Inicia a nossa aplicação Java 
+
+public static void main (String args []){
+
+System.out.println ("gay");
+System.out.println ("gay2");
+System.out.println ("gay3");
+}
+
+}
